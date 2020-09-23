d804be0369c4f0508a01367bd4bd0db228bf218c
Laura <lauravelazquez.lnv@gmail.com>
2020-09-23 10:20:37 -0300

    Obstacles

---
+++ Assets/maderitas.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class maderitas : MonoBehaviour
+{
+    public static int maderas;
+
+    public Text _text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maderas = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {    
+        _text = GetComponent<Text>();
+        
+        _text.text=("Wood:"+maderas);
+
+    }
+}
---
+++ Assets/player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 4f;
+    Rigidbody2D rb2d;
+    Vector2 mov;
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb2d.MovePosition(rb2d.position + mov * speed * Time.deltaTime);
+        mov = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+        );
+        Animations ();
+    }
+    void Animations () {
+
+        if (mov != Vector2.zero) {
+            anim.SetFloat("movX", mov.x);
+            anim.SetFloat("movY", mov.y);
+          //  anim.SetBool("walking", true);
+        } else {
+           // anim.SetBool("walking", false);
+        }
+
+      
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        maderitas.maderas++;
+        Destroy(col.gameObject);
+    }
+}
